12fcc5d461fbe5b6774cdd7c99c373d0930f51f9
hackerman103020 <94463996+hackerman103020@users.noreply.github.com>
2023-09-18 20:28:06 -0500

    Update usb_hid_autofire.c

--- usb_hid_autofire.c
+++ usb_hid_autofire.c
@@ -46,7 +46,7 @@ static void usb_hid_autofire_render_callback(Canvas* canvas, void* ctx) {
 
     canvas_set_font(canvas, FontSecondary);
     canvas_draw_str(canvas, 100, 10, "v");
-    canvas_draw_str(canvas, 106, 10, All[3]");
+    canvas_draw_str(canvas, 106, 10, All[3]);
     canvas_draw_str(canvas, 0, 22, "Press [ok] for auto clicking");
     canvas_draw_str(canvas, 0, 45, "delay [ms]:               [down] = off");
     canvas_draw_str(canvas, 0, 54, "up = switch left/right");
