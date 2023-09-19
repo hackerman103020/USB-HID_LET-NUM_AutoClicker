a8323437813b032d9d15d9f0ecbd1cca39f86d67
hackerman103020 <94463996+hackerman103020@users.noreply.github.com>
2023-09-18 21:16:27 -0500

    Update usb_hid_autofire.c

--- usb_hid_autofire.c
+++ usb_hid_autofire.c
@@ -26,9 +26,9 @@ bool btn_select = false;
 bool ison = false;
 int btn_sel = 0; //     0 = off     1 = right     -1 = left
 uint32_t autofire_delay = 10;
-char All[36] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'};
+char Allppe[36] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'};
 int poppo = 32;
- char str[] = All[2];
+ char str[] = Allppe[2];
 
 static void usb_hid_autofire_render_callback(Canvas* canvas, void* ctx) {
     UNUSED(ctx);
