6b9596877d7641b0537c81011699f89a899dc328
hackerman103020 <94463996+hackerman103020@users.noreply.github.com>
2023-09-20 00:46:06 -0500

    Update usb_hid_autofire.c

--- usb_hid_autofire.c
+++ usb_hid_autofire.c
@@ -34,10 +34,8 @@ static void usb_hid_autofire_render_callback(Canvas* canvas, void* ctx) {
     //strcpy(words , "p.1");
     UNUSED(ctx);
     char autofire_delay_str[12];
-    char words_str[12];
     //std::string pi = "pi is " + std::to_string(3.1415926);
     itoa(autofire_delay, autofire_delay_str, 10);
-    itoa(words, words_str, 10);
     //sprintf(autofire_delay_str, "%lu", autofire_delay);
 
 
@@ -54,7 +52,7 @@ static void usb_hid_autofire_render_callback(Canvas* canvas, void* ctx) {
     canvas_draw_str(canvas, 0, 22, "Press [ok] for auto clicking");
     canvas_draw_str(canvas, 0, 45, "delay [ms]:               [down] = off");
     canvas_draw_str(canvas, 0, 54, "up = switch left/right");
-    canvas_draw_str(canvas, 50, 46, words_str);
+    canvas_draw_str(canvas, 50, 46, autofire_delay_str);
     canvas_draw_str(canvas, 0, 63, "Press [back] to exit");
 }
 
