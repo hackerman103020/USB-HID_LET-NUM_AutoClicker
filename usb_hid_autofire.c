6caf707eac4562b4299e92dfd25424cf4205d177
hackerman103020 <94463996+hackerman103020@users.noreply.github.com>
2023-09-20 19:28:26 -0500

    Update usb_hid_autofire.c

--- usb_hid_autofire.c
+++ usb_hid_autofire.c
@@ -204,12 +204,12 @@ int32_t usb_hid_autofire_app(void* p) {
         if(btn_left_autofire) {
             //furi_hal_hid_mouse_press(HID_MOUSE_BTN_LEFT);
            // furi_hal_hid_kb_press(HID_KEYBOARD_DELETE);
-             furi_hal_hid_kb_press(KEY_A);
+             furi_hal_hid_kb_press(0x04);
             // TODO: Don't wait, but use the timer directly to just don't send the release event (see furi_hal_cortex_delay_us)
             furi_delay_us(autofire_delay * 500);
             //furi_hal_hid_mouse_release(HID_MOUSE_BTN_LEFT);
             // furi_hal_hid_kb_release(HID_KEYBOARD_DELETE);
-            furi_hal_hid_kb_release(KEY_A);
+            furi_hal_hid_kb_release(0x04);
             furi_delay_us(autofire_delay * 500); 
         
 
